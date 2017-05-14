06c2d66212457a6a3d9401bbe93025565b1c565b
Matheus <mathfrance@hotmail.com>
2017-05-14 19:04:49 -0300

    Card Horizontal

--- src/main/webapp/js/init.js
+++ src/main/webapp/js/init.js
@@ -1,19 +1,19 @@
-(function($){
-  $(function(){
-    $('.modal').modal();
-    $('.barralateral').sideNav('hide');
-    $('.barralateral').sideNav({
-       menuWidth: 300, // Default is 240
-       edge: 'left', // Choose the horizontal origin
-       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-    $('.collapsible').collapsible();
-    $('.carousel').carousel({
-      full_width: true,
-      dist: 60
-    });
-    window.setInterval(function(){
-      $('.carousel').carousel('next')
-    },5000)
-  }); // end of document ready
+(function($) {
+    $(function() {
+        $('.modal').modal();
+        $('.barralateral').sideNav('hide');
+        $('.barralateral').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        });
+        $('.collapsible').collapsible();
+        $('.carousel').carousel({
+            full_width: true,
+            dist: 50,
+        });
+        window.setInterval(function() {
+            $('.carousel').carousel('next')
+        }, 5000)
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
