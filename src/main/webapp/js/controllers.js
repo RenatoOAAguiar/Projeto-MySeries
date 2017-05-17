6d83e5609ad37803692e9effdd15a7758f50360f
Matheus <mathfrance@hotmail.com>
2017-05-17 03:15:25 -0300

    Add da descrição da série

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,60 +1,66 @@
-angular.module('MySeries').controller('SideNavCtrl', function($scope){
-})
+angular.module('MySeries').controller('SideNavCtrl', function($scope) {})
 
-.controller('SerieCtrl', function($scope, $http, $routeParams, $rootScope, UrlGetService, EpisodesService, IdImdbService){
+.controller('SerieCtrl', function($scope, $http, $routeParams, $rootScope, UrlGetService, EpisodesService, IdImdbService) {
     $scope.titulo = "Top Series";
     $scope.poster = '';
     var nome = $routeParams.nome;
-    var url = "http://www.omdbapi.com/?t="+ nome +"&plot=full";
+    var url = "http://www.omdbapi.com/?t=" + nome + "&plot=full";
     var dadosSeason = [];
     var dados = {};
     $scope.badge = 10;
-    
+
     $('#comentario').trigger('autoresize');
-    UrlGetService.getUrl(url).then(function (response){
+    UrlGetService.getUrl(url).then(function(response) {
         dados = response.data;
         $scope.poster = dados.Poster;
         $scope.titulo = dados.Title;
         $scope.descricao = dados.Plot;
         $scope.nota = dados.Ratings[0].Value;
         $scope.atores = dados.Actors;
+        $scope.duracao = dados.Runtime;
+        $scope.ano = dados.Year;
+        $scope.genero = dados.Genre;
+        $scope.diretor = dados.Director;
+        $scope.escritor = dados.Writer;
+        $scope.pais = dados.Country;
         var totalSeason = dados.totalSeasons;
         $scope.premiacao = dados.Awards;
+        $scope.qtdVotos = dados.imdbVotes;
         $scope.id = dados.imdbID;
         var episodios = [];
-        for(i = 1 ; i < totalSeason; i ++){
-            var urlSeason = "http://www.omdbapi.com/?t="+$routeParams.nome+"&Season="+i;
-            EpisodesService.getUrl(urlSeason).then(function (response){
+        for (i = 1; i < totalSeason; i++) {
+            var urlSeason = "http://www.omdbapi.com/?t=" + $routeParams.nome + "&Season=" + i;
+            EpisodesService.getUrl(urlSeason).then(function(response) {
                 episodios.push(response.data.Episodes)
             })
         }
         $scope.episodios = episodios;
 
-       });
-     $scope.calculaAvaliacao = function(aval){
-         if(aval == 1){
-             $scope.badge ++;
-         } else {
-             $scope.badge --;
-         }
-     }
+    });
+    $scope.calculaAvaliacao = function(aval) {
+        if (aval == 1) {
+            $scope.badge++;
+        } else {
+            $scope.badge--;
+        }
+    }
 
     $scope.dadosEpisodio = function(id) {
         $scope.enviaId(id);
         $rootScope.$emit("buscarEpisodio", {});
     }
 
-    $scope.enviaId = function(id){
+    $scope.enviaId = function(id) {
         IdImdbService.setProperty(id);
     };
-    
+
 
 })
 
-.controller('LoginCtrl', function($scope, $timeout, cfpLoadingBar){
-    $scope.login = function(){
+.controller('LoginCtrl', function($scope, $timeout, cfpLoadingBar) {
+    $scope.login = function() {
         cfpLoadingBar.start();
-        $timeout(function () {
+        $timeout(function() {
             cfpLoadingBar.complete();
             $('#userSettings').removeClass('hide');
             $('#linkLogin').addClass('hide');
@@ -66,40 +72,40 @@ angular.module('MySeries').controller('SideNavCtrl', function($scope){
         delete $scope.senha;
     }
 
-    $scope.validaCampos = function(){
-        if($scope.usuario == "" || $scope.senha == "" || $scope.usuario == undefined || $scope.senha == undefined){
+    $scope.validaCampos = function() {
+        if ($scope.usuario == "" || $scope.senha == "" || $scope.usuario == undefined || $scope.senha == undefined) {
             return true;
-        } else{
+        } else {
             return false;
         }
     }
 
-    $scope.habilitarPerfil = function(){
+    $scope.habilitarPerfil = function() {
         $scope.perfilOk == true;
     }
 })
 
-.controller('PrincipalCtrl', function ($scope, $compile){
-     $scope.titulo = "Top Series";
-     $('#comentario').trigger('autoresize');
-     window.setInterval(function() {
-            $('.carousel').carousel('next')
-     }, 5000);
+.controller('PrincipalCtrl', function($scope, $compile) {
+    $scope.titulo = "Top Series";
+    $('#comentario').trigger('autoresize');
+    window.setInterval(function() {
+        $('.carousel').carousel('next')
+    }, 5000);
 })
 
-.controller('CriticasCtrl', function($scope){
+.controller('CriticasCtrl', function($scope) {
     $scope.titulo = "Críticas";
 })
 
-.controller('DetalheSerieCtrl', function($scope, $rootScope, EpisodesService, IdImdbService){
-    $rootScope.$on("buscarEpisodio", function(){
-           $scope.buscarEpisodio();
+.controller('DetalheSerieCtrl', function($scope, $rootScope, EpisodesService, IdImdbService) {
+    $rootScope.$on("buscarEpisodio", function() {
+        $scope.buscarEpisodio();
     });
-     
-     $scope.buscarEpisodio = function(){
-         id = IdImdbService.getProperty();
-         var url = "http://www.omdbapi.com/?i="+id;
-         EpisodesService.getUrl(url).then(function (response){
+
+    $scope.buscarEpisodio = function() {
+        id = IdImdbService.getProperty();
+        var url = "http://www.omdbapi.com/?i=" + id;
+        EpisodesService.getUrl(url).then(function(response) {
             console.log(url);
             console.log(response);
             $('#modaldetalhe').modal('open')
@@ -109,10 +115,10 @@ angular.module('MySeries').controller('SideNavCtrl', function($scope){
             $scope.seasonEp = response.data.Episodio;
             $scope.posterEpisodio = response.data.Poster;
             $scope.sinopseEpisodio = response.data.Plot;
-         });
-     }
+        });
+    }
 
-     $scope.fecharModal = function(){
-         $('#modaldetalhe').modal('close');
-     }
+    $scope.fecharModal = function() {
+        $('#modaldetalhe').modal('close');
+    }
 })
