47afb405e7db8b459a1d686569e2c7ecb37852ee
Matheus <mathfrance@hotmail.com>
2017-05-23 20:08:30 -0300

    icons

--- src/main/webapp/js/funcoes.js
+++ src/main/webapp/js/funcoes.js
@@ -2,34 +2,29 @@ function trocarIcone(nome) {
     if (nome == "img/icons/fullPlus-icon.png") {
         if (document.getElementById("iconeAdd").src == "http://localhost:9090/Projeto-MySeries/img/icons/fullPlus-icon.png") {
             document.getElementById("iconeAdd").src = "http://localhost:9090/Projeto-MySeries/img/icons/plus-icon.png";
-        }
-        else {
+        } else {
             document.getElementById("iconeAdd").src = nome;
         }
-    }
-    else if (nome == "img/icons/check-icon.png") {
-        if (document.getElementById("iconEye").src == "http://localhost:9090/Projeto-MySeries/img/icons/check-icon.png") {
+    } else if (nome == "img/icons/check-icon.png") {
+        if (document.getElementById("iconEye").src == "Projeto-MySeries/img/icons/check-icon.png") {
             document.getElementById("iconEye").src = "http://localhost:9090/Projeto-MySeries/img/icons/eye-icon.png";
-        }
-        else {
+        } else {
             document.getElementById("iconEye").src = nome;
         }
-    }
-    else if (nome == "img/icons/fullHeart-icon.png") {
+    } else if (nome == "img/icons/fullHeart-icon.png") {
         if (document.getElementById("iconeHeart").src == "http://localhost:9090/Projeto-MySeries/img/icons/fullHeart-icon.png") {
             document.getElementById("iconeHeart").src = "http://localhost:9090/Projeto-MySeries/img/icons/empytHeart-icon.png";
-        }
-        else {
+        } else {
             document.getElementById("iconeHeart").src = nome;
         }
     }
 }
+
 function contador() {
     if (nome == "img/icons/checkEP-icon.png") {
         if (document.getElementById("iconeEP").src == "http://localhost:9090/Projeto-MySeries/img/icons/checkEP-icon.png") {
             document.getElementById("iconeEP").src = "http://localhost:9090/Projeto-MySeries/img/icons/eyeEP-icon.png";
-        }
-        else {
+        } else {
             document.getElementById("iconeEP").src = nome;
         }
     }
