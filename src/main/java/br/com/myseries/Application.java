ddb1629126033a4929fdc49471af5dedb098df1e
Renato Aguiar <renatooaa@outlook.com>
2017-07-07 07:49:18 -0300

    Teste heroku.

--- src/main/java/br/com/myseries/Application.java
+++ src/main/java/br/com/myseries/Application.java
@@ -1,22 +1,19 @@
 package br.com.myseries;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+//public class Application {
+//
+//  public static void main(String[] args) {
+//    SpringApplication.run(Application.class, args);
+//  }
+//}
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-}
-
-/*
-----Utilizar para servidor standalone(Sem spring-boot:run)
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
 }
 
-*/
